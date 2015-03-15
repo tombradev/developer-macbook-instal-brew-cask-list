f6aff7b12ed4b682338b8212a613fbad65731f34
tombradev <tombradev@gmail.com>
2015-03-15 20:02:14 +0700

    Update installConfig.sh

--- installConfig.sh
+++ installConfig.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
-#
+# This is a clean install for developer macbook, the list of apps and dependencies are totally based on my personal.
+# Please feel free to change or adding some more list into the file, therefore we are able to create a perfect developer machine for all kinds of developer.
+# One mac to rule them all!
 # Brew packages that I use alot.
 #
 
+
+#Dependencies
 #brew install wget
 #brew install homebrew/dupes/tidy
-#brew install homebrew/php/php56
-#brew install homebrew/php/php56-xdebug
-#brew install homebrew/php/phpsh
 #brew install fish
 #brew install ffmpeg
 
 #Programming Language
 brew install node
 
+#php
+brew install homebrew/php/php56
+brew install homebrew/php/php56-xdebug
+brew install homebrew/php/phpsh
+
 #database
 brew install mysql
 
-#
+# CASK PACKAGE 
 # Some cask packages that I like.
 #
 
