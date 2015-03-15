465f6aae7f0de03fcc51f93ce85b69a236db3565
tombradev <MH@MH.local>
2015-03-15 19:17:18 +0700

    List of package 2015
    This is the list of package installed on my new clean mac 2015, please
    feel free to use and change the list in order to create a perfect
    developer machine.
    
    One developer machine to rule them all!

---
+++ installConfig.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+#
+# Brew packages that I use alot.
+#
+
+#brew install wget
+#brew install homebrew/dupes/tidy
+#brew install homebrew/php/php56
+#brew install homebrew/php/php56-xdebug
+#brew install homebrew/php/phpsh
+#brew install fish
+#brew install ffmpeg
+
+#Programming Language
+brew install node
+
+#database
+brew install mysql
+
+#
+# Some cask packages that I like.
+#
+
+#dependencies
+brew cask install xquartz
+
+#file Sharing
+brew cask install dropbox
+
+#texteditor
+brew cask install sublime-text
+brew cask install brackets
+brew cask install atom
+
+#productivity
+brew cask install alfred
+brew cask install controlplane
+brew cask install iterm2
+brew cask install sequel-pro
+brew cask install the-unarchiver
+brew cask install github
+brew cask install sourcetree
+
+#internet Browsers
+brew cask install google-chrome
+brew cask install firefox
+
+#entertainment
+brew cask install inkscape
+brew cask install vlc
+brew cask install rdio
+
+#communication
+brew cask install skype
+brew cask install slack
+
+#Infastructure
+brew cask install virtualbox
+brew cask install vagrant
